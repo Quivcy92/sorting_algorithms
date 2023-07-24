0ac43f5362fd4e2804815e3d1aaab5a3f463cf2c
Quincy92 <sotyphilips@yahoo.com>
2023-07-24 07:58:45 -0700

    6. Counting sort

--- sort.h
+++ sort.h
@@ -51,6 +51,6 @@ void merge_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void quick_s(int *array, int lo, int hi, size_t size);
-
+void counting_sort(int *array, size_t size);
 
 #endif
